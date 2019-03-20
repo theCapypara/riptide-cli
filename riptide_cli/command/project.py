ff009791b72f776e64972c030d79ac1ea2c55af2
Marco Köpcke <marco.koepcke@tudock.de>
2019-03-20 13:26:23 +0100

    start:fg to start services interactively

--- riptide_cli/command/project.py
+++ riptide_cli/command/project.py
@@ -15,6 +15,7 @@ def load(ctx):
     """Adds project commands to the CLI, if project commands are available"""
     if "project" in ctx.system_config:
         ctx.command.add_command(start,    'start')
+        ctx.command.add_command(start_fg, 'start-fg')
         ctx.command.add_command(stop,     'stop')
         ctx.command.add_command(restart,  'restart')
         ctx.command.add_command(cmd,      'cmd')
@@ -55,6 +56,76 @@ async def start(ctx, services):
     if services is not None:
         services = services.split(",")
     await start_project(ctx, services)
+
+
+@cli_section("Service")
+@click.command(context_settings={
+    'ignore_unknown_options': True  # Make all unknown options redirect to arguments
+})
+@click.pass_context
+@click.option('--services', '-s', required=False, help='Names of services to start, comma-separated (default: all)')
+@click.argument('interactive_service', required=True)
+@click.argument('arguments', required=False, nargs=-1, type=click.UNPROCESSED)
+@async_command(interrupt_handler=interrupt_handler)
+async def start_fg(ctx, services, interactive_service, arguments):
+    """
+    Starts services and then runs a service in foreground.
+
+    When using this command, Riptide will start all your services
+    as normal except for one service you specify.
+
+    This one service will be started in foreground mode. Input and Outputs
+    will be attached to the current console.
+    You can pass additional arguments to add to the service command.
+    This will basically run the service as it were a command.
+    This allows for interactive debugging of services.
+
+    If the INTERACTIVE_SERVICE is already started, it is stopped first.
+
+    When running a service in foreground mode the logging options
+    for stdout and stderr are ignored.
+    stdout and stderr are printed to console instead.
+
+    Following options are also ignored:
+
+    - pre_start
+
+    - post_start
+
+    - roles.src (is set)
+
+    - working_directory (is set to current working directory)
+
+    """
+
+    if not ctx.parent.project_is_set_up:
+        return please_set_up()
+
+    project = ctx.parent.system_config["project"]
+    engine = ctx.parent.engine
+
+    if "services" not in project["app"] or interactive_service not in project["app"]["services"]:
+        raise RiptideCliError("The service %s was not found." % interactive_service, ctx=ctx)
+
+    if services is not None:
+        normal_services = services.split(",")
+    elif "services" in project["app"]:
+        normal_services = list(project["app"]["services"].keys())
+    else:
+        normal_services = []
+
+    # Remove interactive service from normal service list
+    if interactive_service in normal_services:
+        normal_services.remove(interactive_service)
+
+    echo(style("(1/3) Starting other services...", bg='cyan', fg='white'))
+    await start_project(ctx, normal_services, show_status=False)
+
+    echo(style("(2/3) Stopping %s..." % interactive_service, bg='cyan', fg='white'))
+    await stop_project(ctx, [interactive_service], show_status=False)
+
+    echo(style("(3/3) Starting in %s foreground mode..." % interactive_service, bg='cyan', fg='white'))
+    engine.service_fg(project, interactive_service, arguments)
 
 
 @cli_section("Service")
