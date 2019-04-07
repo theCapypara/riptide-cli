4aad756bb06dd279bafcaa18a2c7d3d600d95161
Parakoopa <parakoopa@live.de>
2019-04-07 23:10:58 +0200

    Changes to setup.py

--- setup.py
+++ setup.py
@@ -4,10 +4,18 @@ setup(
     name='riptide_cli',
     version='0.1',
     packages=find_packages(),
-    include_package_data=True,
+    description= 'TODO',  # TODO
+    long_description= 'TODO',  # TODO
     install_requires=[
+        'riptide_lib == 0.1',
         'Click >= 7.0',
-        # TODO
+        'colorama >= 0.4',
+        'click-help-colors >= 0.5',
+        'tqdm >= 4.29',
+    ],
+    # TODO
+    classifiers=[
+        'Programming Language :: Python',
     ],
     entry_points='''
         [console_scripts]
