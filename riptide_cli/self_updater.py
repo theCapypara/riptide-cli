fbc423203ace30cb0ed57311904cc1cd4236cbb8
Marco Köpcke <marco.koepcke@tudock.de>
2019-06-07 11:04:11 +0200

    Self-Updater: Also update configcrunch

--- riptide_cli/self_updater.py
+++ riptide_cli/self_updater.py
@@ -9,6 +9,7 @@ def update():
     print("Updating riptide packages via pip...")
     print()
     packages = [dist.project_name for dist in pkg_resources.working_set if dist.project_name.startswith('riptide-')]
+    packages.append('configcrunch')
     call("pip3 install --upgrade " + ' '.join(packages), shell=True)
 
 
